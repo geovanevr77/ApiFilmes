56248ff1e411e48bb51b3724b0683a1c827cafa9
Geovane <geovaneviana.df@gmail.com>
2023-10-23 00:28:02 -0300

    metodos post e get

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,28 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace FilmesApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilme()
+        {
+            return filmes;
+        }
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O preenchimento é obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O preenchimento é no máximo de 50 caracteres.")]
+        public string Titulo { get; set; }
+        
+        [Required(ErrorMessage = "O preenchimento é obrigatório.")]
+        [MaxLength(40, ErrorMessage = "O preenchimento é no máximo de 40 caracteres.")]
+        public string Genero { get; set; }
+        
+        [Required(ErrorMessage = "O preenchimento é obrigatório.")]
+        [Range(70, 600, ErrorMessage ="A duração do filme deve ser entre 70 e 600 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
